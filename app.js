3c66a371f71118a75bcafe05d677a84487355781
Alex Crooks <alexcrooks2@gmail.com>
2021-10-10 00:51:03 -0700

    Almost done, need to find bug

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose")
+const db = require("./config/keys").mongoURI;
+const users = require('./routes/api/users')
+const tweets = require('./routes/api/tweets')
+const User = require('./models/User')
+const bodyParser = require('body-parser');
+const passport = require('passport')
+
+
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log('Connected to mongoDB'))
+.catch(err => console.log(err))
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "Jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save()
     res.send("Hello World!")
-})
+});
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets) 
 
 // get port variable 
 const port = process.env.PORT || 5000;
